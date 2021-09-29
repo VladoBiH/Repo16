591a8a0439109fb3c30e9b208eb89d1e9804b169
vladimir <vladimir.obradovic.sa@hotmail.com>
2021-09-29 10:20:35 +0200

    last push

---
+++ ex06/src/index.js
@@ -0,0 +1,7 @@
+const firstName = firstName => firstName.toUpperCase();
+const lastName = lastName => lastName.toLowerCase();;
+
+
+
+exports.firstName = firstName;
+exports.lastName = lastName;
---
+++ ex09/src/index.js
@@ -0,0 +1,4 @@
+const Person = require('./person');
+
+const person = new Person('John', 'john@example.com', '35');
+console.log(person.getPerson())
